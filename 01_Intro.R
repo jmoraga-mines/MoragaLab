d39dbd65600195dd6fd885870b5efe653cf64e80
Jaime "Jim" Moraga <47835974+jmoraga-mines@users.noreply.github.com>
2022-05-16 02:30:56 -0700

    Update intro.R

--- 01_Intro.R
+++ 01_Intro.R
@@ -5,18 +5,50 @@ if (!require("pacman")){
 
 pacman::p_load(caret, sf, terra, tidyverse, mlr3verse, pROC, nnet)
 
+### Raster functions to save and do max-min normalization
+doe_writeRaster <- function(x, filename, format="raster", overwrite=TRUE, bandorder="BSQ"){
+  if(tools::file_ext(filename) != "grd") {
+    filename <- tools::file_path_sans_ext(filename)
+    filename <- paste(filename, ".grd", sep="")
+  }
+  f1<-raster::writeRaster(x=x, filename=filename, bandorder=bandorder, 
+                          format=format, overwrite=overwrite)
+  raster::hdr(f1, "ENVI")
+  return(f1)
+}
+
+# Max-min normalization
+normalize_raster <- function(s){
+  mnv <- raster::cellStats(s,'min', na.rm=TRUE)
+  mxv <- raster::cellStats(s,'max', na.rm=TRUE)
+  x <- (s - mnv) / (mxv - mnv)
+  return(x)
+}
+
+
+
+
+
 # Contains original PSInSAR (deformation) data
 PSInSAR_df <- readRDS("data/PSInSAR_df.rds")
 PSInSAR_sf <- st_as_sf(PSInSAR_df, coords = c("Longitude", "Latitude"), 
                        agr = "constant", crs=4326)
 PSInSAR_sf_n11 <- st_transform(PSInSAR_sf, crs="epsg:32611")
 
+# Contains PSInSAR Inverse Distance Weighted (IDW) interpolation
+idw_3x3 <- raster::raster("data/deformation_3x3.tif")
+idw_30x30 <- raster::raster("data/deformation_30x30.tif")
+idw_mean <- raster::cellStats(idw_3x3, mean)
+idw_sd <- raster::cellStats(idw_3x3, sd)
+hymap <- raster::raster("data/HyMapFull_osp")
+
 # We will use these variables to split geothermal/non-geothermal
 v_mean <- mean(PSInSAR_df$Velocity)
 v_sd <- sd(PSInSAR_df$Velocity)
-geo_factor <- 1   # We can change this to get more strict on what is or not geothermal
+geo_factor <- 0.5   # We can change this to get more strict on what is or not geothermal
 
 m <- raster::stack("data/brady_ai_stack")
+
 m <- m[[c("Temperature", "Faults", "Slope", 
           "Chalcedony", "Kaolinite", "Gypsum", "Hematite")]]
 
@@ -42,12 +74,13 @@ PSInSAR_df_n11$Geothermal <- ifelse(PSInSAR_df_n11$Velocity<(v_mean-geo_factor*v
                                     )
 Geothermal_df <- subset(PSInSAR_df_n11, select=-Velocity)
 
-rm(df, PSInSAR_sf, PSInSAR_sf_n11, PSInSAR_df, PSInSAR_df_n11, m, m_sf)
+rm(PSInSAR_sf, PSInSAR_sf_n11, PSInSAR_df, PSInSAR_df_n11, m, m_sf)
+rm(separated_coord)
 gc() # Garbage collector, frees up memory
 
 ########       Spatiotemporal sampling from here and on...
 new_ai_data_xydf <- na.omit(Geothermal_df)
-head(new_ai_data_df)
+head(new_ai_data_xydf)
 
 # Convert to factor
 new_ai_data_xydf$Geothermal <- factor(ifelse(new_ai_data_xydf$Geothermal>=0.5, "Yes", "No"),
@@ -62,13 +95,13 @@ task_st_classif <- mlr3spatiotempcv::TaskClassifST$new(new_ai_data_xydf,
                                                                          coordinate_names=c("x", "y"))
                                                        )
 # Creates spatiotemporal resampling. 5 folds, repeated 2 times
-resampling_spcv <- mlr3::rsmp('repeated_spcv_coords', folds = 10, repeats = 4)
+resampling_spcv <- mlr3::rsmp('repeated_spcv_coords', folds = 8, repeats = 5)
 
 # The answer to the ultimate question of life, the universe, and everything
 set.seed(42)
 
 # Neural network - 7 hidden neuron
-nnet_learner <- mlr3::lrn("classif.nnet", size = 7, maxit = 1000) # Neural network, with hidden neurons
+nnet_learner <- mlr3::lrn("classif.nnet", size = 7, maxit = 2000) # Neural network, with hidden neurons
 
 
 print(nnet_learner)
@@ -82,9 +115,29 @@ nn_sp$score(measures = c(msr("classif.acc"), msr("classif.bacc"), msr("classif.f
 nn_sp$aggregate(measures = c(msr("classif.acc"), msr("classif.bacc"), msr("classif.fbeta")))
 
 ####################
-nnet_14 <- nn_sp$learners[[14]]$model
-m <- raster::stack("data/brady_ai_stack")
-prediction_raster <- raster::predict(m, nnet_14)
-raster::spplot(prediction_raster)
-raster::spplot(prediction_raster<0.5)
+nnet_best <- nn_sp$learners[[3]]$model
+b <- raster::stack("data/brady_minerals_stack")
+d <- raster::stack("data/desert_minerals_stack")
+prediction_raster_brady <- raster::predict(b, nnet_best)
+prediction_raster_desert <- raster::predict(d, nnet_best)
+#raster::spplot(prediction_raster)
+raster::spplot(prediction_raster_brady<0.5, main="Brady Model 3")
+raster::spplot(prediction_raster_desert<0.5, main="Desert Model 3")
 
+# AUC: Area under the curve. 1 is perfect, 0.5 is equal to fully random
+pROC::auc(predictor=values(prediction_raster_brady), response=values(b$Geothermal), na.rm=T)
+pROC::auc(predictor=values(prediction_raster_desert), response=values(d$Geothermal), na.rm=T)
+# ROC: Receiver Operating Characteristic curve
+# a diagonal line is equal to a random response (50% chance of picking the right answer)
+# a curve reaching the top left is perfect classification
+pROC::roc(predictor=values(prediction_raster_brady), response=values(b$Geothermal), na.rm=T, plot=T)
+pROC::roc(predictor=values(prediction_raster_desert), response=values(d$Geothermal), na.rm=T, plot=T)
+
+# Create a prediction and reference data frame
+pred <- data.frame(reference = values(b$Geothermal), data = values(prediction_raster_brady))
+pred_cutoff <- 0.5
+pred$reference <- factor(ifelse(pred$reference>pred_cutoff, "Yes", "No"))
+pred$data <- factor(ifelse(pred$data<pred_cutoff, "Yes", "No"))
+
+# Calculate statistics based on Confusion Matrix
+caret::confusionMatrix(data=pred$data, reference=pred$reference, positive="Yes")
